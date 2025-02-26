feec48c8eb14aa21bce1d221b8652c42148b0657
Roshan Kewat <roshankewat9090@gmail.com>
2025-02-26 22:36:25 +0530

    Initial Backend Commit

---
+++ backend/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+});
